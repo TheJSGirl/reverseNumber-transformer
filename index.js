88d43434894aa2df7013f86cd4844da3dd4029ce
Priyanka <prinegi100@gmail.com>
2019-10-03 13:16:04 +0530

    created a file numList.txt

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const file = fs.createWriteStream('numList.txt');
+
+
+function readFile() {
+  file.on('error', function(err) { /* error handling */ });
+  file.write("124");
+  file.write("\n");
+  file.write("4567");
+  file.write("\n");
+  file.write("234");
+  file.write("\n");
+  file.write("545");
+  file.write("\n");
+  file.write("678");
+  file.end();
+}
+
+readFile();
